8d0b7c87f8c5b613b6cbdeecb8b48bd0b57d4afd
Semyon <138588831+MrSemy@users.noreply.github.com>
2023-10-09 00:45:52 +0800

    Add files via upload

---
+++ project/simpleapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class PostsList(ListView):
+    model = Post
+    ordering = '-date_of_post'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+
+class PostDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Post
+    # Используем другой шаблон — post.html
+    template_name = 'post.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'post'
+# Create your views here.
